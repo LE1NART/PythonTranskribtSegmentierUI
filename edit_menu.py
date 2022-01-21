a70aea73427f21f40c323ed60796ca66eea784fb
Linus vA <Linus@von-Au.de>
2022-01-21 18:41:55 +0100

    Bearbeiten Menu komplett eingebaut, shortcuts habe ich die bereits integrierten in windows genutzt, bzw. clipboard

--- edit_menu.py
+++ edit_menu.py
@@ -4,7 +4,7 @@ from tkinter import *
 
 
 class editmenu(tkinter.Menu):
-   def __init__(self,menu,master):
+    def __init__(self,menu,master):
         super().__init__(menu, tearoff=0)
       
         
@@ -12,8 +12,29 @@ class editmenu(tkinter.Menu):
         self.add_command(label="Rückgängig" ,command=lambda: master.textfield.edit_undo() , accelerator="Crtl+Z")
         self.add_command(label="Wiederherstellen",command=lambda: master.textfield.edit_redo(), accelerator="Crtl+Y")
         self.add_separator()
-        self.add_command(label="Ausschneiden")#,command=TODO)
-        self.add_command(label="Kopiere")#,command=TODO)
-        self.add_command(label="Einfügen")#,command=TODO)
-        self.add_command(label="Löschen")#,command=TODO)
-        self.add_command(label="Alles auswählen")#,command=TODO)
+        self.add_command(label="Ausschneiden",command=lambda: self.__cut(master), accelerator="Crtl+X")
+        self.add_command(label="Kopiere",command=lambda: self.__copy(master), accelerator="Crtl+C")
+        self.add_command(label="Einfügen",command=lambda: self.__paste(master), accelerator="Crtl+V")
+        self.add_command(label="Löschen",command=lambda: self.__delete(master), accelerator="ENTF")
+        self.add_command(label="Alles auswählen",command=lambda: self.__selectAll(master), accelerator="Crtl+A")
+        
+        
+    def __cut(self,master):
+        self.__copy(master)
+        master.textfield.delete(tkinter.SEL_FIRST,tkinter.SEL_LAST)
+        
+    def __copy(self,master):
+        if master.textfield.selection_get():
+            master.clipboard_clear()
+            master.clipboard_append(master.textfield.selection_get())
+            
+
+    
+    def __paste(self,master):
+        master.textfield.insert(master.textfield.index(INSERT),master.clipboard_get())
+        
+    def __delete(self,master):
+        master.textfield.delete(tkinter.SEL_FIRST,tkinter.SEL_LAST)
+       
+    def __selectAll(self,master):
+        master.textfield.tag_add('sel',1.0,'end')
